d8f2551565c7e52a7978c23587a23d344f728a9e
SphaeriCactus <sphaericactus@gmail.com>
2020-07-10 15:26:51 -0700

    Camera movement done

---
+++ GMTK 2020 Game/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    private Transform playerTransform;
+
+    private float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        playerTransform = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerTransform.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
