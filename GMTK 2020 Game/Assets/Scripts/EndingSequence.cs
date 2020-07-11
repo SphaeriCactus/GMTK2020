08bf0267a7b621db588ee80d9d96828e7c230838
Snowy <devsnowgames@gmail.com>
2020-07-12 01:37:32 +0200

    Buttom addedede

---
+++ GMTK 2020 Game/Assets/Scripts/EndingSequence.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndingSequence : MonoBehaviour
+{
+
+    public TextMeshProUGUI controlText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Ending());
+    }
+
+    IEnumerator Ending()
+    {
+        yield return new WaitForSeconds(2);
+
+        //Remove "Control. \n"
+        for(int i = 0; i < 9; i++)
+        {
+            controlText.text = controlText.text.Remove(0, 1);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
