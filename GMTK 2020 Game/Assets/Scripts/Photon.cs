62ca01bca5ab492f01e2f078006ae761b35de844
Snowy <devsnowgames@gmail.com>
2020-07-11 03:08:28 +0200

    Added photon

---
+++ GMTK 2020 Game/Assets/Scripts/Photon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Photon : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            //Do shit
+        }
+        else
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 500);
+        }
+    }
+}
