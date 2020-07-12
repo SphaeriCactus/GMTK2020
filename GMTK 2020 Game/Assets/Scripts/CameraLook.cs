e591fd05d53cd8ea575e6fd1239b6b2ee0ef147e
SphaeriCactus <sphaericactus@gmail.com>
2020-07-12 10:05:22 -0700

    Stuff

--- GMTK 2020 Game/Assets/Scripts/CameraLook.cs
+++ GMTK 2020 Game/Assets/Scripts/CameraLook.cs
@@ -19,10 +19,10 @@ public class CameraLook : MonoBehaviour
         hasLooked = false;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
