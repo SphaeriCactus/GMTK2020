dcbdb72f4521403c2dd401c79b9a431844d4bc08
SphaeriCactus <sphaericactus@gmail.com>
2020-07-11 17:35:04 -0700

    Added button functionality

---
+++ GMTK 2020 Game/Assets/Scripts/Buttom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttom : MonoBehaviour
+{
+    private bool inside = false;
+    public Edward edward;
+    public CameraLook cameraMovement;
+    public PlayerController playerMovement;
+    void Update()
+    {
+        if (inside && Input.GetKeyDown("e"))
+        {
+            StartCoroutine("Press");
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inside = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            inside = false;
+        }
+    }
+
+    IEnumerator Press()
+    {
+        edward.Speak(5);
+        yield return new WaitForSeconds(18.1f);
+        cameraMovement.enabled = false;
+        playerMovement.enabled = false;
+        yield return new WaitForSeconds(6.5f);
+        SceneManager.LoadScene("End");
+    }
+}
